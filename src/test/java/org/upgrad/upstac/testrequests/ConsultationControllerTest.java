fb7d2970f420fbd50591d46619131e622f947c47
Gopal <s10gopal@gmail.com>
2020-12-30 20:58:38 +0530

    Implemented LabRequestController and ConsultationController

--- src/test/java/org/upgrad/upstac/testrequests/ConsultationControllerTest.java
+++ src/test/java/org/upgrad/upstac/testrequests/ConsultationControllerTest.java
@@ -1,6 +1,7 @@
 package org.upgrad.upstac.testrequests;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.util.Contracts;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,8 @@ import org.springframework.web.server.ResponseStatusException;
 import org.upgrad.upstac.testrequests.TestRequest;
 import org.upgrad.upstac.testrequests.consultation.ConsultationController;
 import org.upgrad.upstac.testrequests.consultation.CreateConsultationRequest;
+import org.upgrad.upstac.testrequests.consultation.DoctorSuggestion;
+import org.upgrad.upstac.testrequests.lab.CreateLabResult;
 import org.upgrad.upstac.testrequests.lab.TestStatus;
 import org.upgrad.upstac.testrequests.RequestStatus;
 import org.upgrad.upstac.testrequests.TestRequestQueryService;
@@ -19,132 +22,192 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 @SpringBootTest
 @Slf4j
 class ConsultationControllerTest {
 
+  @Autowired ConsultationController consultationController;
 
-    @Autowired
-    ConsultationController consultationController;
+  @Autowired TestRequestQueryService testRequestQueryService;
 
+  @Test
+  @WithUserDetails(value = "doctor")
+  public void
+      calling_assignForConsultation_with_valid_test_request_id_should_update_the_request_status() {
 
-    @Autowired
-    TestRequestQueryService testRequestQueryService;
+    TestRequest testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_COMPLETED);
 
+    // Implement this method
 
-    @Test
-    @WithUserDetails(value = "doctor")
-    public void calling_assignForConsultation_with_valid_test_request_id_should_update_the_request_status(){
+    // Create another object of the TestRequest method and explicitly assign this object for
+    // Consultation using assignForConsultation() method
+    // from consultationController class. Pass the request id of testRequest object.
 
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_COMPLETED);
+    TestRequest result = consultationController.assignForConsultation(testRequest.getRequestId());
 
-        //Implement this method
+    // Use assertThat() methods to perform the following two comparisons
+    //  1. the request ids of both the objects created should be same
+    //  2. the status of the second object should be equal to 'DIAGNOSIS_IN_PROCESS'
+    // make use of assertNotNull() method to make sure that the consultation value of second object
+    // is not null
+    // use getConsultation() method to get the lab result
 
-        //Create another object of the TestRequest method and explicitly assign this object for Consultation using assignForConsultation() method
-        // from consultationController class. Pass the request id of testRequest object.
+    assertThat(testRequest.getRequestId(), equalTo(result.getRequestId()));
+    assertThat(result.getStatus(), equalTo(RequestStatus.DIAGNOSIS_IN_PROCESS));
+    Contracts.assertNotNull(result);
+  }
 
-        //Use assertThat() methods to perform the following two comparisons
-        //  1. the request ids of both the objects created should be same
-        //  2. the status of the second object should be equal to 'DIAGNOSIS_IN_PROCESS'
-        // make use of assertNotNull() method to make sure that the consultation value of second object is not null
-        // use getConsultation() method to get the lab result
+  public TestRequest getTestRequestByStatus(RequestStatus status) {
+    return testRequestQueryService.findBy(status).stream().findFirst().get();
+  }
 
+  @Test
+  @WithUserDetails(value = "doctor")
+  public void calling_assignForConsultation_with_valid_test_request_id_should_throw_exception() {
 
+    Long InvalidRequestId = -34L;
+
+    // Implement this method
+
+    // Create an object of ResponseStatusException . Use assertThrows() method and pass
+    // assignForConsultation() method
+    // of consultationController with InvalidRequestId as Id
+
+    ResponseStatusException result =
+        assertThrows(
+            ResponseStatusException.class,
+            () -> {
+              consultationController.assignForConsultation(InvalidRequestId);
+            });
+
+    // Use assertThat() method to perform the following comparison
+    //  the exception message should be contain the string "Invalid ID"
+
+    assertThat(result.getMessage(), containsString("Invalid ID"));
+  }
+
+  @Test
+  @WithUserDetails(value = "doctor")
+  public void
+      calling_updateConsultation_with_valid_test_request_id_should_update_the_request_status_and_update_consultation_details() {
+
+    TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+
+    // Implement this method
+    // Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to
+    // create the object. Pass the above created object as the parameter
+    // Create another object of the TestRequest method and explicitly update the status of this
+    // object
+    // to be 'COMPLETED'. Make use of updateConsultation() method from labRequestController class
+    // (Pass the previously created two objects as parameters)
+
+    CreateConsultationRequest createConsultationRequest = getCreateConsultationRequest(testRequest);
+    TestRequest result =
+        consultationController.updateConsultation(
+            testRequest.getRequestId(), createConsultationRequest);
+
+    // Use assertThat() methods to perform the following three comparisons
+    //  1. the request ids of both the objects created should be same
+    //  2. the status of the second object should be equal to 'COMPLETED'
+    // 3. the suggestion of both the objects created should be same. Make use of getSuggestion()
+    // method to get the results.
+
+    assertThat(testRequest.getRequestId(), equalTo(result.getRequestId()));
+    assertThat(result.getStatus(), equalTo(RequestStatus.COMPLETED));
+    assertThat(
+        createConsultationRequest.getSuggestion(), equalTo(result.consultation.getSuggestion()));
+  }
+
+  @Test
+  @WithUserDetails(value = "doctor")
+  public void calling_updateConsultation_with_invalid_test_request_id_should_throw_exception() {
+
+    TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+
+    // Implement this method
+
+    // Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to
+    // create the object. Pass the above created object as the parameter
+
+    CreateConsultationRequest createConsultationRequest = getCreateConsultationRequest(testRequest);
+
+    // Create an object of ResponseStatusException . Use assertThrows() method and pass
+    // updateConsultation() method
+    // of consultationController with a negative long value as Id and the above created object as
+    // second parameter
+    // Refer to the TestRequestControllerTest to check how to use assertThrows() method
+
+    ResponseStatusException result =
+        assertThrows(
+            ResponseStatusException.class,
+            () -> {
+              consultationController.updateConsultation(-1000001L, createConsultationRequest);
+            });
+
+    // Use assertThat() method to perform the following comparison
+    //  the exception message should be contain the string "Invalid ID"
+
+    assertThat(result.getMessage(), containsString("Invalid ID"));
+  }
+
+  @Test
+  @WithUserDetails(value = "doctor")
+  public void calling_updateConsultation_with_invalid_empty_status_should_throw_exception() {
+
+    TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+
+    // Implement this method
+
+    // Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to
+    // create the object. Pass the above created object as the parameter
+    // Set the suggestion of the above created object to null.
+
+    CreateConsultationRequest createConsultationRequest = getCreateConsultationRequest(testRequest);
+    createConsultationRequest.setSuggestion(null);
+
+    // Create an object of ResponseStatusException . Use assertThrows() method and pass
+    // updateConsultation() method
+    // of consultationController with request Id of the testRequest object and the above created
+    // object as second parameter
+    // Refer to the TestRequestControllerTest to check how to use assertThrows() method
+
+    ResponseStatusException result =
+        assertThrows(
+            ResponseStatusException.class,
+            () -> {
+              consultationController.updateConsultation(
+                  testRequest.getRequestId(), createConsultationRequest);
+            });
+  }
+
+  public CreateConsultationRequest getCreateConsultationRequest(TestRequest testRequest) {
+
+    // Create an object of CreateLabResult and set all the values
+
+    CreateConsultationRequest createConsultationRequest = new CreateConsultationRequest();
+    CreateLabResult createLabResult = new CreateLabResult();
+
+    createLabResult.setBloodPressure(testRequest.labResult.getBloodPressure());
+    createLabResult.setOxygenLevel(testRequest.labResult.getOxygenLevel());
+    createLabResult.setHeartBeat(testRequest.labResult.getHeartBeat());
+    createLabResult.setTemperature(testRequest.labResult.getTemperature());
+    createLabResult.setResult(testRequest.labResult.getResult());
+
+    // if the lab result test status is Positive, set the doctor suggestion as "HOME_QUARANTINE" and
+    // comments accordingly
+    // else if the lab result status is Negative, set the doctor suggestion as "NO_ISSUES" and
+    // comments as "Ok"
+
+    if (createLabResult.getResult().equals(TestStatus.POSITIVE)) {
+      createConsultationRequest.setComments("Home Quarantine");
+      createConsultationRequest.setSuggestion(DoctorSuggestion.HOME_QUARANTINE);
+
+    } else {
+      createConsultationRequest.setComments("Ok");
+      createConsultationRequest.setSuggestion(DoctorSuggestion.NO_ISSUES);
     }
 
-    public TestRequest getTestRequestByStatus(RequestStatus status) {
-        return testRequestQueryService.findBy(status).stream().findFirst().get();
-    }
-
-    @Test
-    @WithUserDetails(value = "doctor")
-    public void calling_assignForConsultation_with_valid_test_request_id_should_throw_exception(){
-
-        Long InvalidRequestId= -34L;
-
-        //Implement this method
-
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass assignForConsultation() method
-        // of consultationController with InvalidRequestId as Id
-
-
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
-
-    }
-
-    @Test
-    @WithUserDetails(value = "doctor")
-    public void calling_updateConsultation_with_valid_test_request_id_should_update_the_request_status_and_update_consultation_details(){
-
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
-
-        //Implement this method
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
-
-        //Create another object of the TestRequest method and explicitly update the status of this object
-        // to be 'COMPLETED'. Make use of updateConsultation() method from labRequestController class (Pass the previously created two objects as parameters)
-
-        //Use assertThat() methods to perform the following three comparisons
-        //  1. the request ids of both the objects created should be same
-        //  2. the status of the second object should be equal to 'COMPLETED'
-        // 3. the suggestion of both the objects created should be same. Make use of getSuggestion() method to get the results.
-
-
-
-    }
-
-
-    @Test
-    @WithUserDetails(value = "doctor")
-    public void calling_updateConsultation_with_invalid_test_request_id_should_throw_exception(){
-
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
-
-        //Implement this method
-
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass updateConsultation() method
-        // of consultationController with a negative long value as Id and the above created object as second parameter
-        //Refer to the TestRequestControllerTest to check how to use assertThrows() method
-
-
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
-
-    }
-
-    @Test
-    @WithUserDetails(value = "doctor")
-    public void calling_updateConsultation_with_invalid_empty_status_should_throw_exception(){
-
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
-
-        //Implement this method
-
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
-        // Set the suggestion of the above created object to null.
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass updateConsultation() method
-        // of consultationController with request Id of the testRequest object and the above created object as second parameter
-        //Refer to the TestRequestControllerTest to check how to use assertThrows() method
-
-
-    }
-
-    public CreateConsultationRequest getCreateConsultationRequest(TestRequest testRequest) {
-
-        //Create an object of CreateLabResult and set all the values
-        // if the lab result test status is Positive, set the doctor suggestion as "HOME_QUARANTINE" and comments accordingly
-        // else if the lab result status is Negative, set the doctor suggestion as "NO_ISSUES" and comments as "Ok"
-        // Return the object
-
-
-        return null; // Replace this line with your code
-
-    }
-
+    // Return the object
+    return createConsultationRequest;
+  }
 }
